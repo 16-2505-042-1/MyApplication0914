0bfac40e62ddef3e5eaeb59669a1261b32699da3
16-2505-042-1 <w042ff@yamaguchi-u.ac.jp>
2018-09-14 16:24:18 +0900

    Initial commit

---
+++ app/src/main/java/e/enpit_p11/myapplication0914/MainActivity.kt
@@ -0,0 +1,12 @@
+package e.enpit_p11.myapplication0914
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
